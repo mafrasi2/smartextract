3a4af69d2182cb8b3a85d15c04c43cbcfccac6a0
Max Sistemich <maximilian.sistemich@rwth-aachen.de>
2020-02-25 21:31:18 +0100

    slightly more powerful archive name recognition

--- src/unpack.rs
+++ src/unpack.rs
@@ -1,3 +1,4 @@
+use std::collections::HashSet;
 use std::path::PathBuf;
 use lazy_static;
 use regex::Regex;
@@ -11,10 +12,12 @@ pub enum UnpackError {
 
 pub fn is_archive(path: &PathBuf) -> bool {
     lazy_static::lazy_static! {
-        static ref RAR_PART_RE: Regex = Regex::new("^part\\d+$").unwrap();
-        static ref RAR_FIRST_PART_RE: Regex = Regex::new("^part0*1$").unwrap();
-        static ref P7Z_PART_RE: Regex = Regex::new("^\\d+$").unwrap();
-        static ref P7Z_FIRST_PART_RE: Regex = Regex::new("^0*1$").unwrap();
+        static ref INFIX_PART_RE: Regex = Regex::new("^part\\d+$").unwrap();
+        static ref INFIX_FIRST_PART_RE: Regex = Regex::new("^part0*1$").unwrap();
+        static ref SUFFIX_PART_RE: Regex = Regex::new("^\\d+$").unwrap();
+        static ref SUFFIX_FIRST_PART_RE: Regex = Regex::new("^0*1$").unwrap();
+
+        static ref ARCHIVE_NAMES: HashSet<&'static str> = ["rar", "7z"].iter().cloned().collect();
     }
 
     let fname = match path.file_name() {
@@ -24,16 +27,14 @@ pub fn is_archive(path: &PathBuf) -> bool {
     let fname = fname.to_string_lossy().to_lowercase();
 
     let parts: Vec<_> = fname.rsplit(".").collect();
-    if parts[0] == "rar" {
-        if parts.len() > 1 {
-            RAR_FIRST_PART_RE.is_match(parts[1]) || !RAR_PART_RE.is_match(parts[1])
-        } else {
+    if ARCHIVE_NAMES.contains(parts[0]) {
+        if parts.len() <= 1 {
             true
+        } else {
+            INFIX_FIRST_PART_RE.is_match(parts[1]) || !INFIX_PART_RE.is_match(parts[1])
         }
-    } else if parts[0] == "7z" {
-        true
-    } else if parts.len() > 1 && parts[1] == "7z" {
-        P7Z_FIRST_PART_RE.is_match(parts[0]) || !P7Z_PART_RE.is_match(parts[0])
+    } else if parts.len() > 1 && ARCHIVE_NAMES.contains(parts[1]) {
+        SUFFIX_FIRST_PART_RE.is_match(parts[0]) || !SUFFIX_PART_RE.is_match(parts[0])
     } else {
         false
     }
@@ -56,12 +57,15 @@ mod tests {
     fn archive_names() {
         assert!(is_archive(&PathBuf::from(".rar")));
         assert!(is_archive(&PathBuf::from("abc.rar")));
+        assert!(is_archive(&PathBuf::from("abc.rar.001")));
+        assert!(!is_archive(&PathBuf::from("abc.rar.002")));
         assert!(is_archive(&PathBuf::from("a.part1.rar")));
         assert!(is_archive(&PathBuf::from("a.part001.rar")));
         assert!(!is_archive(&PathBuf::from("a.part2.rar")));
 
         assert!(is_archive(&PathBuf::from(".7z")));
         assert!(is_archive(&PathBuf::from("a.7z.001")));
+        assert!(is_archive(&PathBuf::from("a.part01.rar")));
         assert!(!is_archive(&PathBuf::from("a.7z.002")));
         assert!(!is_archive(&PathBuf::from("a.7z.010")));
     }
