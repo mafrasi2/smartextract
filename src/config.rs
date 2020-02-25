a5abbb3e9a384399eb2f4e37b120689c2445c76a
Max Sistemich <maximilian.sistemich@rwth-aachen.de>
2020-02-25 17:25:55 +0100

    split up into modules

---
+++ src/config.rs
@@ -0,0 +1,40 @@
+use std::fs::File;
+use std::path::PathBuf;
+use xdg::BaseDirectories;
+use serde::{Serialize, Deserialize};
+use serde_json;
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Config {
+    passwords: Vec<String>,
+}
+
+impl Config {
+    fn default_config() -> Config {
+        Config {
+            passwords: vec![],
+        }
+    }
+
+    fn get_config_file() -> PathBuf {
+        let base_dirs = BaseDirectories::new().unwrap();
+        base_dirs.place_config_file("smartunpack.json").unwrap()
+    }
+
+    pub fn load() -> Self {
+        let config_path = Config::get_config_file();
+        let config = if config_path.exists() {
+            let config_file = File::open(config_path).unwrap();
+            serde_json::from_reader(config_file).unwrap()
+        } else {
+            Config::default_config()
+        };
+        config
+    }
+
+    pub fn store(&self) {
+        let config_path = Config::get_config_file();
+        let config_file = File::create(config_path).unwrap();
+        serde_json::to_writer(config_file, self).unwrap();
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,11 @@
 use std::env;
-use std::fs::File;
-use std::path::PathBuf;
-use xdg::BaseDirectories;
-use serde::{Serialize, Deserialize};
-use serde_json;
 
-#[derive(Serialize, Deserialize, Debug)]
-struct Config {
-    passwords: Vec<String>,
-}
-
-fn default_config() -> Config {
-    Config {
-        passwords: vec![],
-    }
-}
-
-fn get_config_file() -> PathBuf {
-    let base_dirs = BaseDirectories::new().unwrap();
-    base_dirs.place_config_file("smartunpack.json").unwrap()
-}
-
-fn load_config() -> Config {
-    let config_path = get_config_file();
-    let config = if config_path.exists() {
-        let config_file = File::open(config_path).unwrap();
-        serde_json::from_reader(config_file).unwrap()
-    } else {
-        default_config()
-    };
-    config
-}
-
-fn store_config(config: &Config) {
-    let config_path = get_config_file();
-    let config_file = File::create(config_path).unwrap();
-    serde_json::to_writer(config_file, config).unwrap();
-}
+mod config;
 
 fn main() {
-    let cfg = load_config();
+    let cfg = config::Config::load();
     for arg in env::args() {
         
     }
-    store_config(&cfg);
+    cfg.store();
 }
