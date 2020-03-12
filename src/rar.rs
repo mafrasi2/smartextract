d890d8a6c9c9750709bc09455f580def8ebfce3d
Max Sistemich <maximilian.sistemich@rwth-aachen.de>
2020-03-12 16:16:16 +0100

    fixed warnings of rar unpacker

--- src/rar.rs
+++ src/rar.rs
@@ -4,6 +4,6 @@ use crate::archives::Archive;
 use crate::passwords::PasswordDatabase;
 use crate::temp_unpack::{Unpack, UnpackError};
 
-pub fn unpack_rar<P: AsRef<Path>>(archive: &Archive, to: P, pdb: &PasswordDatabase, overwrite: bool) -> Result<Unpack, UnpackError> {
+pub fn unpack_rar<P: AsRef<Path>>(_archive: &Archive, _to: P, _pdb: &PasswordDatabase, _overwrite: bool) -> Result<Unpack, UnpackError> {
     unimplemented!("rar extraction");
 }
